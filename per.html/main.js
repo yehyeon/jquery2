ac40041f866d273bd8ba97f98edad67631526c43
choe <sooeed@naver.com>
2017-04-28 17:17:20 +0900

    k
    k

---
+++ per.html/main.js
@@ -0,0 +1,24 @@
+// https://ianlunn.co.uk/plugins/jquery-parallax/
+// https://github.com/IanLunn/jQuery-Parallax
+$(function(){
+     var section = $('#contents > .parallax > div');
+     var sectionInfo = [];
+
+     section.each(function(i){
+         var tg = $(this);
+         sectionInfo.push(tg.offset().top);
+     });
+
+    section.css('position', 'fixed');
+
+    $(window).scroll(function(){
+        var sct = $(window).scrollTop();
+
+        section.each(function(i){
+            var tg = $(this);
+            var tt = -1 * sct + sectionInfo[i];
+
+            tg.css('top', tt);
+        });
+    });
+});
